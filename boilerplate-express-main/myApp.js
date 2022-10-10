4d0e78e943c9162a1b1b777de011d6974f404623
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-10 22:29:27 +0700

    add : basic node and express 58%

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 console.log("Hello World");
 
@@ -7,12 +8,31 @@ console.log("Hello World");
 //     res.send('Hello Express');
 // })
 
+app.use((req, res, next) => {
+    const logger = `${req.method} ${req.path} - ${req.ip}`
+    console.log(logger);
+    next();
+})
+
 absolutePath = __dirname + '/views/index.html';
 
 app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 });
 
-app.use('/public' , express.static(__dirname + '/public'));
+// app.get('/json' , (req,res) => {
+//     res.json({"message" : "Hello json"})
+// })
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({ "message": "HELLO JSON" })
+    } else {
+        res.json({ "message": "Hello json" })
+    }
+})
+
+
+app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
